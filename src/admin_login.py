89a917686ec482f4f4038adcdd85bb1fc70c086d
RPPython <pr79@abv.bg>
2024-06-27 12:37:09 +0300

    class_admin

--- src/admin_login.py
+++ src/admin_login.py
@@ -1,2 +1,17 @@
 # Administrator Login Function
 
+class Admin:
+    def __init__(self) -> None:
+        self.admin_login()
+
+    def admin_login(self):
+        print("Administrator Login")
+        self.username = input("Enter your username: ")
+        self.pin_code = input("Enter your pin_code ")
+        self.logged_in = False
+
+        if self.username == "admin" and self.pin_code == "1329":  # Example username and pin code
+            print("\nHello! Welcome, to the user managment system!")
+            self.logged_in = True
+        else:
+            print("Invalid login data. Please try again.")
