96c1828077992bfb51136a7734d1ebb0d89460dd
RPPython <pr79@abv.bg>
2024-06-28 17:19:06 +0300

    changing_data_handler

--- data/data_handler.py
+++ data/data_handler.py
@@ -3,31 +3,28 @@ users_db = [
         'name': 'Svetlin',
         'surname': 'Sofroniev',
         'date_of_birth' : '14/1/1976',
-        'address_city' : 'Sofia',
-        'address_street' : 'Pleven',
-        'address_number' : '6',
-        'pin_code' : '0001',
-        'username' : 'ss'
+        'address' : 'Sofia, Str. Pleven 6',
+        'username' : 'ss' ,
+        'pin_code' : '0001'
+        
     },
     {
         'name': 'Mariya',
         'surname': 'Mersinkova',
         'date_of_birth' : '15/9/1970',
-        'address_city' : 'Shumen',
-        'address_street' : 'Nancho Popovich',
-        'address_number' : '35',
-        'pin_code' : '0002',
-        'username' : 'mm'
+        'address' : 'Shumen,Str. Nancho Popovich 35a',
+        'username' : 'mm' ,
+        'pin_code' : '0002'
+        
     },
     {
         'name': 'Pavel',
         'surname': 'Zlatanov',
         'date_of_birth' : '20/5/1993',
-        'address_city' : 'Sofia',
-        'address_street' : 'Usmivka',
-        'address_number' : '2',
-        'pin_code' : '0003',
-        'username' : "pz"
+        'address' : 'Sofia, Str. Usmivka 2',
+        'username' : "pz",
+        'pin_code' : '0003'
+        
     }
 ]
 
